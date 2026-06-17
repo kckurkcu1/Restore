ec1e94f1d2008883022eb3099964997886e76d73
Kerem Can Kürkçü <kerem.kurkcu@hotmail.com>
2026-06-17 16:50:49 +0300

    x

---
+++ API/Data/Migrations/20260617134947_OrderEntityAdded.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderEntityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BuyerEmail = table.Column<string>(type: "TEXT", nullable: false),
+                    ShippingAddress_Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ShippingAddress_Line1 = table.Column<string>(type: "TEXT", nullable: false),
+                    ShippingAddress_Line2 = table.Column<string>(type: "TEXT", nullable: true),
+                    ShippingAddress_City = table.Column<string>(type: "TEXT", nullable: false),
+                    ShippingAddress_State = table.Column<string>(type: "TEXT", nullable: false),
+                    ShippingAddress_PostalCode = table.Column<string>(type: "TEXT", nullable: false),
+                    ShippingAddress_Country = table.Column<string>(type: "TEXT", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Subtotal = table.Column<long>(type: "INTEGER", nullable: false),
+                    DeliveryFee = table.Column<long>(type: "INTEGER", nullable: false),
+                    Discount = table.Column<long>(type: "INTEGER", nullable: false),
+                    PaymentIntentId = table.Column<string>(type: "TEXT", nullable: false),
+                    OrderStatus = table.Column<int>(type: "INTEGER", nullable: false),
+                    PaymentSummary_Last4 = table.Column<int>(type: "INTEGER", nullable: false),
+                    PaymentSummary_Brand = table.Column<string>(type: "TEXT", nullable: false),
+                    PaymentSummary_ExpMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    PaymentSummary_ExpYear = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ItemOrdered_ProductId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ItemOrdered_Name = table.Column<string>(type: "TEXT", nullable: false),
+                    ItemOrdered_PictureUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<long>(type: "INTEGER", nullable: false),
+                    Quantity = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrderId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItem_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e069461a-10cf-4abf-9930-d070b2a7e40f",
+                column: "ConcurrencyStamp",
+                value: "1097eadd-9c9d-419a-a8e6-d84fadc37b10");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ed2e9149-fa53-484c-a93f-bd33f9e9fcf6",
+                column: "ConcurrencyStamp",
+                value: "c1939722-4ec6-4153-93b6-9b858eb4f6b2");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItem_OrderId",
+                table: "OrderItem",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItem");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e069461a-10cf-4abf-9930-d070b2a7e40f",
+                column: "ConcurrencyStamp",
+                value: "26619508-2571-4d3c-828c-13d240c3bcb2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ed2e9149-fa53-484c-a93f-bd33f9e9fcf6",
+                column: "ConcurrencyStamp",
+                value: "66bb7bf1-92c7-4c90-81df-ab70c54be703");
+        }
+    }
+}
